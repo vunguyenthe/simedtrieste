66b450018df859dfd405a3d37c7a8135ee9008bc
Vu Nguyen <vunguyenthe1976@gmail.com>
2018-03-08 21:33:14 +0700

    removed unsed code

--- src/main/java/com/small/business/config/ThymeleafConfig.java
+++ src/main/java/com/small/business/config/ThymeleafConfig.java
@@ -24,7 +24,6 @@ public class ThymeleafConfig {
     // private Environment env;
     private static final String DRIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
     //private static final String URL = "jdbc:mysql://localhost:3306/test";
-    private static final String URL_LOGDB = "logdb.url";
     //private static final String USER_NAME = "root";
     //private static final String PASSWORD = "123456";
     private static final String URL = "jdbc:mysql://simedtrieste:3306/sampledb";
