78d33f71c95ec27bbf40b98057a4f46347eba7f9
Vu Nguyen <vunguyenthe1976@gmail.com>
2018-04-14 01:44:44 +0700

    hello test

--- src/main/java/com/small/business/controller/UserController.java
+++ src/main/java/com/small/business/controller/UserController.java
@@ -1,51 +1,22 @@
 package com.small.business.controller;
 
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.small.business.model.user.User;
-import com.small.business.service.user.UserService;
 
 @Controller
 @RequestMapping("/api")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    @RequestMapping(value = "/helloTest", method = RequestMethod.GET)
+    public @ResponseBody String helloTest(@PathVariable long id) {
 
-    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
-    public @ResponseBody User getUser(@PathVariable long id) {
-
-        return userService.getUserById(id);
+        return new String("Hello");
     }
 
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public @ResponseBody List<User> getUserList() {
 
-        return userService.getAllUser();
-    }
-
-    @RequestMapping(value = "/user", method = RequestMethod.POST)
-    public @ResponseBody long addUser(@RequestBody User user) {
-        return userService.addUser(user);
-    }
-
-    @RequestMapping(value = "/user", method = RequestMethod.PUT)
-    public @ResponseBody boolean updateUser(@RequestBody User user) {
-        return userService.updateUser(user);
-    }
-
-    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
-    public @ResponseBody boolean removeUser(@PathVariable("id") Long id) {
-
-        return userService.deleteUserById(id);
-    }
 
 }
